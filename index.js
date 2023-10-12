ca93b5716ee2f7685d76915276eeaea5957a2f25
camilodvl <camilo.228@hotmail.com>
2023-10-11 22:36:19 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import app from "./server.js"
+
+app.listen(8080, ()=>{
+    console.log("Server on port 8080")
+})
---
+++ server.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const app=express(); //creacion del servidor
+
+export default app
